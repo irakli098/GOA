54da67277aedcd69e309f59d09b51b57da19ecde
irakli098 <iraklisomkhishvili76@gmail.com>
2024-10-28 12:12:44 +0400

    GOA

---
+++ level 18/classwork/classwork01.py
@@ -0,0 +1,31 @@
+# i = 0
+
+# # condition - პირობა
+
+# while i < 10:
+#     print(i)
+
+#     i += 1
+
+
+# 1) მომხმარებელს შეეკითხეთ სახელი სანამ ეს სახელი არიქნება გაბრიელი მაქამდე არ გაჩერდეს და შეეკითხოს ისევ ხელახლა თუ იქნება გაბრიელი გაჩერდეს
+
+
+
+# name = input("enter name: ")
+# while name == "gabrieli":
+#     print("gabrieli")
+# if name != "gabrieli":
+#     print(input("enter name:"))
+
+
+a = input("enter your name ")
+i = 1 
+while i < 2:
+    if a == "luka":
+        i = i + 1
+        print("you can acses")
+    else:
+        print("try agfain")
+ 
+
