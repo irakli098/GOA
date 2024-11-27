de9c658654c5bde7f1f1f88b78f3e1ad3f9c6b7d
irakli098 <iraklisomkhishvili76@gmail.com>
2024-11-27 17:16:33 +0400

    GOA

---
+++ level 30/homework/homework01.py
@@ -0,0 +1,56 @@
+# 1) მომხმარებელს შეეკითხეთ ორი რიცხვი შემდეგ კი შექმენით ფუნქცია რომელსაც არგუმენტად გადასცემთ ამ ორ რიცხვს ხოლო ფუნქცია დააბრუნებს ამ ორი რიცხვის ჯამს, ასევე გააკეთე ასეთი 4 ფუნქცია სხვადასხვა მათემატიკური მოქმედებებისთვის, გამოიყენეთ პარამეტრები და არგუმენტად გადაეცით მომხარებლის შემოტანილი რიცხვები
+
+# num = int(input("enter a num: "))
+# num1 = int(input("enter a num: "))
+
+# def sum(a, b):
+#     return a + b
+# print(sum(num, num1))
+
+# def min(a, b):
+#     return a - b
+# print(min(num, num1))
+
+# def multiply(a, b):
+#     return a * b
+# print(multiply(num, num1))
+
+
+# def divide(a, b):
+#     return a / b
+# print(divide(num, num1))
+
+# 3) შექმენით ფუნქცია რომელსაც გადაეცემა არგუმენტად რიცხვი შემდეგ კი მან უნდა დაგვიბრუნოს ლუწია ეს რიცხვი თუ კენტი
+
+
+# user_num = int(input("enter a number: "))
+
+# def even_or_odd(number):
+#     if number %2 == 0:
+#         return "even"
+#     else:
+#         return "odd"
+    
+# print(even_or_odd(user_num))
+
+# 4) შექმენით ფუნქცია რომელსაც არგუმენტად გადაეცემა რიცხვი შემდეგ კი მან უნდა დაგვიბრუნოს ეს რიცხვი დადებითია თუ უარყოფითი
+
+
+# num = int(input("enter a num: "))
+
+# def positive_or_negative(number):
+#     if number > 0:
+#         return "positive"
+#     elif number == 0:
+#         return "zero"
+#     else:
+#         return "negative"
+    
+
+# 5) შექმენით ფუნქცია რომელსაც არგუმენტად გადაეცემა რიცხვი შემდეგ კი ფუნქციამ უნდა დაგვიბრუნოს მისი საპირისპირო რიცხვი
+
+# num = int(input("enter a num: "))
+
+# def opposite(number):
+#     return number * -1
+# print(opposite(num))
